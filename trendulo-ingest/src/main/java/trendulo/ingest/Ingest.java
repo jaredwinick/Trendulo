dfe62d6e97a5d48773d0fc17f073dabab50d5c1a
Jared Winick <jaredwinick@gmail.com>
2012-01-22 20:45:59 -0700

    Functionality to read from a Twitter JSON file as a source and generate timestamped n-grams

--- trendulo-ingest/src/main/java/trendulo/ingest/Ingest.java
+++ trendulo-ingest/src/main/java/trendulo/ingest/Ingest.java
@@ -1,3 +1,19 @@
+// =================================================================================================
+// Copyright 2012 Jared Winick
+// -------------------------------------------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this work except in compliance with the License.
+// You may obtain a copy of the License in the LICENSE file, or at:
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =================================================================================================
+
 package trendulo.ingest;
 
 import org.springframework.context.ApplicationContext;
@@ -11,8 +27,15 @@ public class Ingest {
 	public static void main(String[] args) {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext( "applicationContext.xml" );
-		Hello hello = (Hello) context.getBean( "hello" );
-		hello.sayHello();
+		final TemporalNGramSource nGramSource = (TemporalNGramSource) context.getBean( "nGramSource" );
+		Thread nGramSourceThread = new Thread( nGramSource );
+		nGramSourceThread.start();
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+		    public void run() { 
+		    	nGramSource.shutdown();
+		     }
+		 });
 	}
 
 }
