065020f19829e5241491a74b8cd1fe02047cdbb9
Jared Winick <jaredwinick@gmail.com>
2012-04-05 19:55:58 -0600

    removed MONTH granularity as it isn't useful

--- trendulo-ingest/src/main/java/trendulo/ingest/MutationGenerator.java
+++ trendulo-ingest/src/main/java/trendulo/ingest/MutationGenerator.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.iterators.LongCombiner;
 import org.apache.hadoop.io.Text;
 import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
@@ -14,6 +15,18 @@ import org.joda.time.DateTimeZone;
 public class MutationGenerator {
 
 	private static Logger log = Logger.getLogger( MutationGenerator.class );
+	
+	private static Value oneValueFixedLen;
+	private static Value oneValueVLen;
+	
+	static {
+		try {
+			oneValueFixedLen = new Value( Value.longToBytes( 1 ) );
+			oneValueVLen = new Value( LongCombiner.VAR_LEN_ENCODER.encode( 1l ) );
+		} catch (IOException e) {
+			log.error( "Can't initialize Values",  e );
+		}
+	}
 	
 	/**
 	 * Generate a list of Accumulo Mutations for the TemporalNGram. The n-gram string is
@@ -25,14 +38,50 @@ public class MutationGenerator {
 	public static Mutation generateMutation( TemporalNGram temporalNGram ) {
 		
 		Mutation mutation = new Mutation( new Text( temporalNGram.getnGram() ) );
+
+		//mutation.put( new Text( "MONTH" ), new Text( timestampToMonthString( temporalNGram.getTimestamp() ) ), new Value( Value.longToBytes( 1 ) ) );
+		mutation.put( new Text( "DAY" ), new Text( timestampToDayString( temporalNGram.getTimestamp() ) ), oneValueFixedLen );
+		mutation.put( new Text( "HOUR" ), new Text( timestampToHourString( temporalNGram.getTimestamp() ) ), oneValueFixedLen );
 		
+		return mutation;
+	}
+	
+	public static Mutation generateMutation( TemporalNGram temporalNGram, Long count ) {
+		
+		Mutation mutation = new Mutation( new Text( temporalNGram.getnGram() ) );
+
 		try {
-			mutation.put( new Text( "MONTH" ), new Text( timestampToMonthString( temporalNGram.getTimestamp() ) ), new Value( Value.longToBytes( 1 ) ) );
-			mutation.put( new Text( "DAY" ), new Text( timestampToDayString( temporalNGram.getTimestamp() ) ), new Value( Value.longToBytes( 1 ) ) );
-			mutation.put( new Text( "HOUR" ), new Text( timestampToHourString( temporalNGram.getTimestamp() ) ), new Value( Value.longToBytes( 1 ) ) );
-		} catch (IOException e) {
+		//mutation.put( new Text( "MONTH" ), new Text( timestampToMonthString( temporalNGram.getTimestamp() ) ), new Value( Value.longToBytes( 1 ) ) );
+		mutation.put( new Text( "DAY" ), new Text( timestampToDayString( temporalNGram.getTimestamp() ) ), 
+				new Value( Value.longToBytes( count ) ) );
+		mutation.put( new Text( "HOUR" ), new Text( timestampToHourString( temporalNGram.getTimestamp() ) ), 
+				new Value( Value.longToBytes( count ) ) );
+		}
+		catch ( IOException e ) {
 			log.error( e );
 		}
+		return mutation;
+	}
+	
+	
+	public static Mutation generateMutationVLen( TemporalNGram temporalNGram ) {
+		
+		Mutation mutation = new Mutation( new Text( temporalNGram.getnGram() ) );
+		
+		mutation.put( new Text( "DAY" ), new Text( timestampToDayString( temporalNGram.getTimestamp() ) ), oneValueVLen );
+		mutation.put( new Text( "HOUR" ), new Text( timestampToHourString( temporalNGram.getTimestamp() ) ), oneValueVLen );
+		
+		return mutation;
+	}
+	
+	public static Mutation generateMutationVLen( TemporalNGram temporalNGram, Long count ) {
+		
+		Mutation mutation = new Mutation( new Text( temporalNGram.getnGram() ) );
+		
+		mutation.put( new Text( "DAY" ), new Text( timestampToDayString( temporalNGram.getTimestamp() ) ),
+				new Value( LongCombiner.VAR_LEN_ENCODER.encode( count ) ) );
+		mutation.put( new Text( "HOUR" ), new Text( timestampToHourString( temporalNGram.getTimestamp() ) ), 
+				new Value( LongCombiner.VAR_LEN_ENCODER.encode( count ) ) );
 		
 		return mutation;
 	}
