29f575f736f03ab987a90e0c88a39181ff72d131
Jared Winick <jaredwinick@gmail.com>
2012-04-23 20:41:33 -0600

    additional tests added

--- trendulo-webapp/src/test/java/trendulo/web/util/DateConverterTest.java
+++ trendulo-webapp/src/test/java/trendulo/web/util/DateConverterTest.java
@@ -1,8 +1,12 @@
 package trendulo.web.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
 
 import org.junit.Test;
+
+import trendulo.web.date.DateConverter;
 
 public class DateConverterTest {
 
@@ -27,62 +31,80 @@ public class DateConverterTest {
 	@Test 
 	public void getStartDateString1Days() {
 		// timestamp is Human time (GMT): Fri, 02 Mar 2012 01:00:00 GMT
-		String startDate = DateConverter.getStartDateString( 1, 1330650000000l );
+		//String startDate = DateConverter.getStartDateString( 1, 1330650000000l );
+		String startDate = DateConverter.getStartDateString( 1, 1330650000000l + 21600000l );
 		assertEquals( startDate, "2012030101" );
 	}
 	@Test 
 	public void getStartDateString7Days() {
 		// timestamp is Human time (GMT): Thu, 08 Mar 2012 01:00:00 GMT
-		String startDate = DateConverter.getStartDateString( 7, 1331168400000l );
+		//String startDate = DateConverter.getStartDateString( 7, 1331168400000l );
+		String startDate = DateConverter.getStartDateString( 7, 1331168400000l + 21600000l );
 		assertEquals( startDate, "2012030101" );
 	}
 	@Test 
 	public void getStartDateString30Days() {
 		// timestamp is Human time (GMT): Sat, 31 Mar 2012 01:00:00 GMT
-		String startDate = DateConverter.getStartDateString( 30, 1333155600000l );
+		//String startDate = DateConverter.getStartDateString( 30, 1333155600000l );
+		String startDate = DateConverter.getStartDateString( 30, 1333155600000l + 21600000l );
 		assertEquals( startDate, "2012030101" );
 	}
 	@Test 
 	public void getStartDateString60Days() {
 		// timestamp is Human time (GMT): Sat, 31 Mar 2012 01:00:00 GMT
-		String startDate = DateConverter.getStartDateString( 60, 1333155600000l );
+		//String startDate = DateConverter.getStartDateString( 60, 1333155600000l );
+		String startDate = DateConverter.getStartDateString( 60, 1333155600000l + 21600000l );
 		assertEquals( startDate, "20120131" );
 	}
 	@Test 
 	public void getStartDateString90Days() {
 		// timestamp is Human time (GMT): Sat, 31 Mar 2012 01:00:00 GMT
-		String startDate = DateConverter.getStartDateString( 90, 1333155600000l );
+		//String startDate = DateConverter.getStartDateString( 90, 1333155600000l );
+		String startDate = DateConverter.getStartDateString( 90, 1333155600000l + 21600000l );
 		assertEquals( startDate, "20120101" );
 	}
 	
 	@Test 
 	public void getEndDateString1Days() {
 		// timestamp is Human time (GMT): Fri, 02 Mar 2012 01:00:00 GMT
-		String endDate = DateConverter.getEndDateString( 1, 1330650000000l );
+		//String endDate = DateConverter.getEndDateString( 1, 1330650000000l );
+		String endDate = DateConverter.getEndDateString( 1, 1330650000000l + 21600000l );
 		assertEquals( endDate, "2012030201" );
 	}
 	@Test 
 	public void getEndDateString7Days() {
 		// timestamp is Human time (GMT): Thu, 08 Mar 2012 01:00:00 GMT
-		String endDate = DateConverter.getEndDateString( 7, 1331168400000l );
+		//String endDate = DateConverter.getEndDateString( 7, 1331168400000l );
+		String endDate = DateConverter.getEndDateString( 7, 1331168400000l + 21600000l );
 		assertEquals( endDate, "2012030801" );
 	}
 	@Test 
 	public void getEndDateString30Days() {
 		// timestamp is Human time (GMT): Sat, 31 Mar 2012 01:00:00 GMT
-		String endDate = DateConverter.getEndDateString( 30, 1333155600000l );
+		//String endDate = DateConverter.getEndDateString( 30, 1333155600000l );
+		String endDate = DateConverter.getEndDateString( 30, 1333155600000l + 21600000l );
 		assertEquals( endDate, "2012033101" );
 	}
 	@Test 
 	public void getEndDateString60Days() {
 		// timestamp is Human time (GMT): Sat, 31 Mar 2012 01:00:00 GMT
-		String endDate = DateConverter.getEndDateString( 60, 1333155600000l );
+		//String endDate = DateConverter.getEndDateString( 60, 1333155600000l );
+		String endDate = DateConverter.getEndDateString( 60, 1333155600000l + 21600000l );
 		assertEquals( endDate, "20120331" );
 	}
 	@Test 
 	public void getEndDateString90Days() {
 		// timestamp is Human time (GMT): Sat, 31 Mar 2012 01:00:00 GMT
-		String endDate = DateConverter.getEndDateString( 90, 1333155600000l );
+		//String endDate = DateConverter.getEndDateString( 90, 1333155600000l );
+		String endDate = DateConverter.getEndDateString( 90, 1333155600000l + 21600000l );
 		assertEquals( endDate, "20120331" );
+	}
+	
+	@Test
+	public void getDateStringsForRange() {
+		List<String> dateStrings = DateConverter.getDateStringsForRange( "2012040100", "2012040123", 1 );
+		assertEquals( dateStrings.size(), 24 );
+		assertEquals( dateStrings.get(0), "2012040100" );
+		assertEquals( dateStrings.get(23), "2012040123" );
 	}
 }
